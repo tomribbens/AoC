76dbf4ac85a27193a16c8be65090a5495a27d84e
Tom Ribbens <tom@tomribbens.be>
2021-12-08 18:39:48 +0100

    Re-implemented part 2 in a better way.

--- 2021/day08/second-part.py
+++ 2021/day08/second-part.py
@@ -7,21 +7,7 @@ import sys
 with open(str(sys.argv[1])) as input_file:
     input_array = [[digit.strip().split(' ') for digit in line.strip().split('|')] for line in input_file] 
 
-digits= {}
-digits[0] = set("abcefg")
-digits[1] = set("cf")
-digits[2] = set("acdeg")
-digits[3] = set("acdfg")
-digits[4] = set("bcdf")
-digits[5] = set("abdfg")
-digits[6] = set("abdefg")
-digits[7] = set("acf")
-digits[8] = set("abcdefg")
-digits[9] = set("abcdfg")
-
 total = 0
-
-
 for i in input_array:
     examples = {}
     examples[5] = []
@@ -41,45 +27,39 @@ for i in input_array:
         elif len(j) == 7:
             examples[7] = set(j)
 
-    solution = {}
+    digits = {}
+    # A digit with two segments is a one, three segments is a seven, four segments is a four, and 7 segments is an eight
+    digits[1] = examples[2]
+    digits[7] = examples[3]
+    digits[4] = examples[4]
+    digits[8] = examples[7]
 
-    solution['a'] = examples[2].symmetric_difference(examples[3])
-    solution['d'] = examples[4].intersection(examples[5][0]).intersection(examples[5][1]).intersection(examples[5][2])
-    solution['b'] = examples[4].symmetric_difference(examples[2]).symmetric_difference(solution['d'])
-    
-    for x in examples[6]:
-        if not solution['d'].issubset(x):
-            continue
-        elif examples[2].issubset(x):
-            solution['e'] = x.symmetric_difference(examples[7])
-        else:
-            solution['c'] = x.symmetric_difference(examples[7])
-
-    solution['f'] = solution['c'].symmetric_difference(examples[2])
-    solution['g'] = examples[7].symmetric_difference(solution['a']).symmetric_difference(solution['b']).symmetric_difference(solution['c']).symmetric_difference(solution['d']).symmetric_difference(solution['e']).symmetric_difference(solution['f'])
-
-
-    search = ""
-    replace = ""
-    for k, s in dict(solution).items():
-        replace += k
-        search += ''.join(s)
-
-    trTable = str().maketrans(search, replace)
+    for ex in examples[5]:
+        # Of the 5 segment digits, only a three shares two segments with a one
+        if len(ex.intersection(digits[1])) == 2:
+            digits[3] = ex
+        elif len(ex.intersection(digits[4])) == 3:
+            digits[5] = ex
+        elif len(ex.intersection(digits[4])) == 2:
+            digits[2] = ex
 
 
-    display = ""
+    for ex in examples[6]:
+        if len(ex.intersection(digits[1])) == 1:
+            digits[6] = ex
+        elif len(ex.intersection(digits[4])) == 4:
+            digits[9] = ex
+        elif len(ex.intersection(digits[4])) == 3:
+            digits[0] = ex
+
+    output = ""
+
     for j in i[1]:
-        toTest = j.translate(trTable)
-
-        for d,segments in digits.items():
-            if set(toTest) == segments:
-                display += str(d)
+        for d,segs in digits.items():
+            if set(segs) == set(j):
+                output += str(d)
                 break
 
-
-    total += int(display)
-
-
-
+    total += int(output)
+    
 print(total)
